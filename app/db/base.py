2013c5e09ab40a1bf7260ca257f292cccccf208d
jsdev305 <jsdev305@gmail.com>
2026-01-11 20:55:13 -0500

    add SQLAlchemy declarative base and initial Operator model

---
+++ app/db/base.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer
+from sqlalchemy.orm import DeclarativeBase
+
+class Base(DeclarativeBase):
+    pass
+
+class Operator(Base):
+    __tablename__ = "operators"
+    id = Column(Integer, primary_key=True)
