2c063ae9ed6304b5c2e768d8e823932ca6001032
jsdev305 <jsdev305@gmail.com>
2026-01-13 15:04:04 -0500

    added db to the service as an argument

--- app/routers/devices.py
+++ app/routers/devices.py
@@ -1,6 +1,14 @@
-from fastapi import APIRouter, status, HTTPException
-
+from fastapi import APIRouter, status, HTTPException, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.db.session import get_db
 from app.schemas.device import DeviceCreate, DeviceResponse, DeviceUpdate
+from app.services.devices import (
+    create_device_service,
+    delete_device_service,
+    get_device_service,
+    get_devices_service,
+    update_device_service,
+)
 
 # TODO
 # schemas
@@ -11,44 +19,46 @@ router = APIRouter(prefix="/devices", tags=["devices"])
 
 # GET , get list of devices
 @router.get("/", response_model=list[DeviceResponse])
-async def get_devices():
+async def get_devices(db: AsyncSession = Depends(get_db)):
     try:
-        return await get_devices_service()
+        return await get_devices_service(db)
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
 
 
 # get device
 @router.get("/{device_id}", response_model=DeviceResponse)
-async def get_device(device_id: int):
+async def get_device(device_id: int, db: AsyncSession = Depends(get_db)):
     try:
-        return await get_device_service(device_id)
+        return await get_device_service(db, device_id)
     except ValueError as e:
         raise HTTPException(status_code=404, detail=str(e))
 
 
 # POST, create a device
 @router.post("/", response_model=DeviceResponse, status_code=status.HTTP_201_CREATED)
-async def create_device(payload: DeviceCreate):
+async def create_device(payload: DeviceCreate, db: AsyncSession = Depends(get_db)):
     try:
-        return await create_device_service(payload)
+        return await create_device_service(db, payload)
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
 
 
 # PATCH update a device , based on id
 @router.patch("/{device_id}", response_model=DeviceResponse)
-async def update_device(device_id: int, payload: DeviceUpdate):
+async def update_device(
+    device_id: int, payload: DeviceUpdate, db: AsyncSession = Depends(get_db)
+):
     try:
-        return await update_device_service(device_id, payload)
+        return await update_device_service(db, device_id, payload)
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
 
 
 # DELETE , delete a device, based on id
 @router.delete("/{device_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_device(device_id: int):
+async def delete_device(device_id: int, db: AsyncSession = Depends(get_db)):
     try:
-        return await delete_device_service(device_id)
+        return await delete_device_service(db, device_id)
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
--- app/services/devices.py
+++ app/services/devices.py
@@ -1,19 +1,26 @@
+from app.models.device import Device
 
-#get 
-async def get_devices_service() -> list[DeviceResponse]:
+
+# get
+async def get_devices_service() -> list[Device]:
     pass
 
-#get single device
-async def get_device_service(device_id: int) -> DeviceResponse:
-    pass
-#post create device
-async def create_device_service(payload) -> DeviceResponse:
+
+# get single device
+async def get_device_service(device_id: int) -> Device:
     pass
 
-#patch
-async def update_device_service(device_id:  int, payload) -> DeviceResponse:
-    pass 
 
-#delete
+# post create device
+async def create_device_service(payload) -> Device:
+    pass
+
+
+# patch
+async def update_device_service(device_id: int, payload) -> Device:
+    pass
+
+
+# delete
 async def delete_device_service(device_id: int):
     pass
