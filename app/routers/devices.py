ef01dd61b217e9fdb2c65246a2f152fa7e22368b
jsdev305 <jsdev305@gmail.com>
2026-01-13 13:07:52 -0500

    feat(devices): add full CRUD routes with schemas and GET single

--- app/routers/devices.py
+++ app/routers/devices.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, status, HTTPException
 
-from app.schemas.device import DeviceCreate, DeviceResponse
+from app.schemas.device import DeviceCreate, DeviceResponse, DeviceUpdate
 
 # TODO
 # schemas
@@ -18,8 +18,17 @@ async def get_devices():
         raise HTTPException(status_code=400, detail=str(e))
 
 
+# get device
+@router.get("/{device_id}", response_model=DeviceResponse)
+async def get_device(device_id: int):
+    try:
+        return await get_device_service(device_id)
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
+
+
 # POST, create a device
-@router.post("/", status_code=status.HTTP_201_CREATED)
+@router.post("/", response_model=DeviceResponse, status_code=status.HTTP_201_CREATED)
 async def create_device(payload: DeviceCreate):
     try:
         return await create_device_service(payload)
@@ -28,8 +37,8 @@ async def create_device(payload: DeviceCreate):
 
 
 # PATCH update a device , based on id
-@router.patch("/{device_id}")
-async def update_device(device_id: str, payload: DeviceUpdate):
+@router.patch("/{device_id}", response_model=DeviceResponse)
+async def update_device(device_id: int, payload: DeviceUpdate):
     try:
         return await update_device_service(device_id, payload)
     except ValueError as e:
@@ -38,7 +47,7 @@ async def update_device(device_id: str, payload: DeviceUpdate):
 
 # DELETE , delete a device, based on id
 @router.delete("/{device_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_device(device_id: str):
+async def delete_device(device_id: int):
     try:
         return await delete_device_service(device_id)
     except ValueError as e:
--- app/schemas/device.py
+++ app/schemas/device.py
@@ -1,15 +1,24 @@
 from pydantic import BaseModel, Field
+from typing import Optional
 from app.enums.device_status import DeviceStatus
 from app.enums.device_kind import DeviceKind
 
-
+# for post
 class DeviceCreate(BaseModel):
     kind: DeviceKind
     status: DeviceStatus = DeviceStatus.OFFLINE
     battery: int = Field(default=100, ge=1, le=100)
 
 
+# for the get response
 class DeviceResponse(BaseModel):
     kind: DeviceKind
     status: DeviceStatus
     battery: int = Field(ge=1, le=100)
+
+
+#for update
+class DeviceUpdate(BaseModel):
+    kind: Optional[DeviceKind]
+    status: Optional[DeviceStatus]
+    battery: Optional[int] = Field(ge=1, le=100)
